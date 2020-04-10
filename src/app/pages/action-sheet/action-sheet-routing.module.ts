a1872c861f565295fce287477e2d915dc8627167
luidimso <luidi_mso@outlook.com>
2020-04-09 22:36:59 -0300

    List and tabs

--- src/app/pages/action-sheet/action-sheet-routing.module.ts
+++ src/app/pages/action-sheet/action-sheet-routing.module.ts
@@ -6,7 +6,16 @@ import { ActionSheetPage } from './action-sheet.page';
 const routes: Routes = [
   {
     path: '',
-    component: ActionSheetPage
+    component: ActionSheetPage,
+    children: [
+      {
+        path: 'alert',
+        loadChildren: '../alert/alert.module#AlertPageModule'
+      },{
+        path: 'init',
+        loadChildren: '../init/init.module#InitPageModule'
+      }
+    ]
   }
 ];
 
