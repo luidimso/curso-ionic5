ba3656eb523301274000f7625f382241cd24590d
luidimso <luidi_mso@outlook.com>
2020-04-10 18:37:16 -0300

    Alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,7 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  title:string;
+
+  constructor(public alert: AlertController) { }
+
+  async presentAlert(){
+    const alert = await this.alert.create({
+      header: "Title",
+      subHeader: "SubTitle",
+      message: "Text",
+      inputs: [
+        {
+          name: 'test1',
+          type: 'text',
+          placeholder: 'Test 1'
+        },
+        {
+          id: 'test2',
+          name: 'test2',
+          type: 'number',
+          value: '123'
+        }
+      ],
+      buttons: ['OK',{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'red',
+        handler: () => {
+
+        }
+      },{
+        text: 'Save',
+        handler: (data) => {
+          this.title = data.test1;
+        }
+      }]
+    });
+    await alert.present();
+  }
 
   ngOnInit() {
   }
